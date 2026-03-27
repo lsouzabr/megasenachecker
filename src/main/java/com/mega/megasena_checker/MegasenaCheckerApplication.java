a8d841f891989c222c8519669b7411d2a06bd85e
leonam-souza <lsouzabr@gmail.com>
2026-03-27 05:29:39 -0400

    Commit inicial

---
+++ src/main/java/com/mega/megasena_checker/MegasenaCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.mega.megasena_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegasenaCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegasenaCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mega/megasena_checker/MegasenaCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mega.megasena_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegasenaCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
